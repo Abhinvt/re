aa12a32d4f8c118b6c67d1b619544ab56b253ea2
Abhinvt <168721718+Abhinvt@users.noreply.github.com>
2024-05-12 11:34:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -25,3 +25,22 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+function Send(){
+
+  Email.send({
+    SecureToken :"775bd6d8-36da-4362-9ea9-e00d8118604f",
+    To : 'atkeys7@gmail.com',
+    From :"admin@notifies.site",
+    Subject : "New Contact",
+    Body : "<br/> <br/> Name: " + document.getElementById("Name").value + "<br/><br/> Phone No: " + document.getElementById("phone").value + "<br/> <br/> Email Id: " + document.getElementById("email").value + "<br/> <br/> Message: " + document.getElementById("message").value 
+
+    })
+
+  .then(
+    message => {
+      if (message==='OK') {
+        swal("Thanks", "We Will Reach To You Soon", "success");
+      }
+    }
+  );
+}
